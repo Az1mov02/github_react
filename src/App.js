ad4dedad876086f2be43640c5688892e07f4ccdf
User <raatbek.azimov1712@gmail.com>
2024-10-16 17:29:23 +0600

    Init push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './App.sass'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0)
+
+    function handleIncrement(){
+        setCount(prev => prev + 1)
+    }
+    function handleDecrement(){
+        setCount(prev => prev - 1)
+    }
+
+    return (
+        <div className="wrapper">
+            <h1 className="heading">
+                {count}
+            </h1>
+            <div className="buttons">
+                <button
+                    className="button"
+                    onClick={handleIncrement}
+                >
+                    +
+                </button>
+                <button
+                    className="button"
+                    onClick={handleDecrement}
+                >
+                    -
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
